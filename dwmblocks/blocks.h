b4209895cb0eec7aec2bf13cc82f6d9519d7bc9e
Theo Henson <theodorehenson@protonmail.com>
2020-06-01 18:37:44 -0400

    Changed dwmblocks volume source

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'", 0, 10},
+	{" ", "amixer -D default sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'", 0, 10},
 	{"", "date +\"%y.%m.%d %H:%M\"",	60,		0},
 };
 
