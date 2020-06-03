402c0b8353effc6f552f660b1d04196f14fff2bc
Theo Henson <theodorehenson@protonmail.com>
2020-06-03 15:27:09 -0400

    Updated blocks

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'", 0, 10},
-	{"", "date +\"%y.%m.%d %H:%M\"",	60,		0},
+	{"", "volume_bar",                       0,                                  10},
+	{"", "date +\"%y.%m.%d %H:%M\"",	60,				      0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
