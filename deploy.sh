e63ffd621bc977b437f49726f78eed555d452cfd
Theo Henson <theodorehenson@protonmail.com>
2020-06-04 16:17:49 -0400

    Comment spotifyd link

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ link () {
 mkd () {
     mkdir $HOME/.config/fish/functions -p
     mkdir $HOME/media/documents -p
-    mkdir $HOME/.config/spotifyd
+    # mkdir $HOME/.config/spotifyd
     mkdir $HOME/.config/gtk-3.0
     mkdir $HOME/.config/picom
     mkdir $HOME/.config/sxhkd
@@ -23,7 +23,7 @@ link .bashrc
 link .xinitrc
 link .gtkrc-2.0
 link .bash_profile
-link .newsboat/urls
+# link .newsboat/urls
 link .newsboat/config
 link media/documents/subs
 link .config/sxhkd/sxhkdrc
@@ -35,6 +35,6 @@ link .config/picom/picom.conf
 link .config/fish/config.fish
 link .config/fish/fish_variables
 link .config/gtk-3.0/settings.ini
-link .config/spotifyd/spotifyd.conf
+# link .config/spotifyd/spotifyd.conf
 link .config/fish/functions/fish_prompt.fish
 link .config/fish/functions/fish_mode_prompt.fish
