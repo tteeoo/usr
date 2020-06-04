9356518cfa231ac21c5f38978323e01ae9477898
Theo Henson <theodorehenson@protonmail.com>
2020-06-03 21:10:28 -0400

    Added temp bar

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,8 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*//*Command*/			/*Update Interval*/	/*Update Signal*/
-	{"",	"volume_bar",			0,			10},
-	{"",	"cpu_bar", 		      	4, 			0},
+	{" ",	"volume_bar",			0,			10},
+	{"",	"cpu_bar", 		      	6, 			0},
+	{"",	"temp_bar", 		      	6, 			0},
 	{"",	"time_bar", 		 	60, 			0},
 };
 
