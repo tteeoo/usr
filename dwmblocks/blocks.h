6b4aac45e615ca347cbb3bea37e7873b6d48e7be
Theo Henson <theodorehenson@protonmail.com>
2020-06-07 21:06:53 -0400

    Reorder blocks

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -2,8 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*//*Command*/			/*Update Interval*/	/*Update Signal*/
 	{"",	"volume_bar",			0,			10},
-	{"",	"cpu_bar", 		      	6, 			0},
 	{"",	"temp_bar", 		      	6, 			0},
+	{"",	"cpu_bar", 		      	6, 			0},
 	{"",	"time_bar", 		 	60, 			0},
 };
 
