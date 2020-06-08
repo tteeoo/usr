5c470a32c4ef6ffafb853123532e564751a561d2
Theo Henson <theodorehenson@protonmail.com>
2020-06-08 11:35:04 -0400

    Change prompt for modified git_status_prompt script

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -6,11 +6,4 @@ function fish_prompt
     echo -n (set_color 83A598)(pwd)' '
 
     echo -n (git_status_prompt)
-
-    set_color -o
-    if test "$USER" = 'root'
-        echo -n (set_color CC241D)'# '
-    end
-    echo -n (set_color 8EC07C)'$ '
-    set_color normal
 end
