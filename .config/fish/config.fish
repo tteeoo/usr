b46ee810812c4c43cc1af1659e832d79db9d5385
Theo Henson <theodorehenson@protonmail.com>
2020-06-10 14:55:57 -0400

    Add history search in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ abbr -a -g sus sudo systemctl
 abbr -a -g sdn sudo shutdown now
 abbr -a -g gof gofmt -s -w -e -l .
 abbr -a -g vw nvim -c VimwikiIndex
+abbr -a -g c 'set fzfcmd ( grep " cmd: " ~/.local/share/fish/fish_history | sed "s/- cmd: //" | fzf -e --color=16 --no-sort ) && echo $fzfcmd && eval $fzfcmd'
 
 # ensure key swap
 setxkbmap -option caps:swapescape
