686fe9ff59d549f414cfee2a41fa3ec6c8373ec7
Theo Henson <theodorehenson@protonmail.com>
2020-06-20 20:05:27 -0400

    Add fish abbreviation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ abbr -a -g sus sudo systemctl
 abbr -a -g sdn sudo shutdown now
 abbr -a -g gof gofmt -s -w -e -l .
 abbr -a -g vw nvim -c VimwikiIndex
+abbr -a -g mi 'make && sudo make install'
 abbr -a -g c fzfcmd
 
 alias fzfcmd='set fzfcmd ( grep " cmd: " ~/.local/share/fish/fish_history | sed "s/- cmd: //" | grep -v fzfcmd | tac | fzf -e --color=16 --no-sort ) && echo $fzfcmd && eval $fzfcmd'
