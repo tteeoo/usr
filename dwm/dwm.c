4a79f657415131756969387ddcc419e208bcfc96
Theo Henson <theodorehenson@protonmail.com>
2020-07-17 00:59:31 -0400

    Attempt to fix swallow

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -512,10 +512,10 @@ unswallow(Client *c)
 	setfullscreen(c, 0);
 	updatetitle(c);
 	arrange(c->mon);
-	c->bw = borderpx;
 	XMapWindow(dpy, c->win);
 	XMoveResizeWindow(dpy, c->win, c->x, c->y, c->w, c->h);
 	setclientstate(c, NormalState);
+	c->bw = borderpx;
 	focus(NULL);
 	arrange(c->mon);
 }
