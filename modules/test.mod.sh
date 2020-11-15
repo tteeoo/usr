62adfefb7d5884abe427301e86e7a32e99376f66
Theo Henson <theodorehenson@protonmail.com>
2020-11-15 14:13:12 -0500

    Implement module files/dirs

--- modules/test.mod.sh
+++ modules/test.mod.sh
@@ -1,9 +1,5 @@
 # test module
 
-test_files() {
-	printf ".test\n"
-}
-
 test_add() {
 	printf "linking \$HOME/.test\n"
 	ln -sf "$USR_DIR/home/.test" "$HOME/.test"
@@ -17,4 +13,12 @@ test_remove() {
 test_info() {
 	printf "this module exists for testing purposes.
 it links a file (.test) into your home directory.\n"
+}
+
+test_dirs() {
+	printf ".test-dir\n"
+}
+
+test_files() {
+	printf ".test\n.test-dir/1\n.test-dir/2"
 }
