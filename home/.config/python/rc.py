820c2267c67e942732c81b6462bcc2d6eaab7c41
Theo Henson <theodorehenson@protonmail.com>
2020-11-15 17:54:58 -0500

    Change python, improve curling, create theo_dirs

--- home/.config/python/rc.py
+++ home/.config/python/rc.py
@@ -2,8 +2,15 @@ import os
 import atexit
 import readline
 
-# get .python_history outta ~!
-histfile = os.path.join(os.path.expanduser("~"), ".local/share/python_history")
+# get .python_history out of home directory:
+base = ""
+try:
+    base = os.environ['XDG_CACHE_HOME']
+except KeyError:
+    base = os.path.join(os.path.expanduser("~"), ".cache")
+
+histfile = os.path.join(base, "python_history")
+
 try:
     readline.read_history_file(histfile)
     readline.set_history_length(1000)
---
+++ modules/theo_dirs.mod.sh
@@ -0,0 +1,45 @@
+# theo_dirs module
+
+: "${XDG_CACHE_HOME:=$HOME/.cache}"
+: "${XDG_DATA_HOME:=$HOME/.local/share}"
+
+theo_dirs_add() {
+	printf "theo_dirs: creating directories\n"
+	mkdir -p "$HOME"/av/wall
+	mkdir -p "$HOME"/av/scrot
+	mkdir -p "$HOME"/docs
+	mkdir -p "$HOME"/src/git/aur
+	mkdir -p "$HOME"/src/git/ghw
+	mkdir -p "$HOME"/src/git/ghf
+	mkdir -p "$HOME"/src/git/ghf
+	mkdir -p "$XDG_DATA_HOME"/pub
+	printf "theo_dirs: linking some wallpapers\n"
+	ln -sf "$USR_DIR"/home/av/wall/bg1.jpg "$HOME"/av/wall/bg1.jpg
+	ln -sf "$USR_DIR"/home/av/wall/bg2.jpg "$HOME"/av/wall/bg2.jpg
+}
+
+theo_dirs_remove() {
+	printf "theo_dirs: removing directories\n"
+	rmdir "$HOME"/av/wall
+	rmdir "$HOME"/av/scrot
+	rmdir "$HOME"/av
+	rmdir "$HOME"/docs
+	rmdir "$HOME"/src/git/aur
+	rmdir "$HOME"/src/git/ghw
+	rmdir "$HOME"/src/git/ghf
+	rmdir "$HOME"/src/git
+	rmdir "$HOME"/src
+	rmdir "$XDG_DATA_HOME"/pub
+}
+
+theo_dirs_info() {
+	printf "Creates directories I (theo) like to have in ~.\n"
+}
+
+theo_dirs_dirs() {
+	printf "av/wall"
+}
+
+theo_dirs_files() {
+	printf "av/wall/bg1.jpg av/wall/bg2.jpg"
+}
