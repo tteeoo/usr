97a22a3264dfb213d524530490c70fde323aead0
Theo Henson <theodorehenson@protonmail.com>
2020-12-22 14:33:15 -0500

    Add dmenu, update dwm

--- modules/theo_suk_arch.mod.sh
+++ modules/theo_suk_arch.mod.sh
@@ -6,11 +6,15 @@ theo_suk_arch_add() {
 
 	printf "theo_suk_arch: compiling software and linking binaries\n"
 	mkdir -p "$HOME"/.local/bin
-	for PROG in dwm st dwmblocks ; do
+	for PROG in dwm st dwmblocks dmenu ; do
 		cd "$USR_DIR/home/src/suk/$PROG" || exit
-		make
-		chmod +x "$PROG"
+		make -j4
 		ln -sf "$USR_DIR/home/src/suk/$PROG/$PROG" "$HOME/.local/bin/$PROG"
+		if [ "$PROG" = "dmenu" ] ; then
+			ln -sf "$USR_DIR/home/src/suk/$PROG/$PROG"_run "$HOME/.local/bin/$PROG"_run
+			ln -sf "$USR_DIR/home/src/suk/$PROG/$PROG"_path "$HOME/.local/bin/$PROG"_path
+			ln -sf "$USR_DIR/home/src/suk/$PROG/stest" "$HOME/.local/bin/stest"
+		fi
 	done
 }
 
@@ -18,7 +22,9 @@ theo_suk_arch_remove() {
 	printf "theo_suk_arch: unlinking %s/src/suk\n" "$HOME"
 	rm "$HOME"/src/suk
 	printf "theo_suk_arch: unlinking binaries\n"
-	rm "$HOME"/.local/bin/st "$HOME"/.local/bin/dwm "$HOME"/.local/bin/dwmblocks
+	rm "$HOME"/.local/bin/st "$HOME"/.local/bin/dwm "$HOME"/.local/bin/dwmblocks \
+		"$HOME"/.local/bin/dmenu "$HOME"/.local/bin/dmenu_run "$HOME"/.local/bin/dmenu_path \
+			"$HOME"/.local/bin/stest
 }
 
 theo_suk_arch_info() {
@@ -26,8 +32,9 @@ theo_suk_arch_info() {
 }
 
 theo_suk_arch_dirs() {
-	printf "src/suk/dwmblocks src/suk/dwm src/suk/st"
+	printf "src/suk/dwmblocks src/suk/dwm src/suk/st src/suk/dmenu"
 }
+
 
 theo_suk_arch_files() {
 	printf "src/suk/dwm/config.h src/suk/dwm/Makefile src/suk/dwm/config.mk
@@ -37,5 +44,9 @@ theo_suk_arch_files() {
 		src/suk/st/win.h src/suk/st/st.h src/suk/st/st.c
 		src/suk/st/x.c src/suk/st/arg.h
 		src/suk/dwmblocks/Makefile src/suk/dwmblocks/blocks.h
-		src/suk/dwmblocks/dwmblocks.c"
+		src/suk/dwmblocks/dwmblocks.c src/suk/dmenu/Makefile
+		src/suk/dmenu/arg.h src/suk/dmenu/config.h src/suk/dmenu/config.mk
+		src/suk/dmenu/dmenu.c src/suk/dmenu/dmenu_path src/suk/dmenu/dmenu_run
+		src/suk/dmenu/drw.c src/suk/dmenu/drw.h src/suk/dmenu/stest.c
+		src/suk/dmenu/util.c src/suk/dmenu/util.h"
 }
